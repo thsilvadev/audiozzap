a4f55035d0307e49ef548c111bebaec26fb33434
thsilvadev <thsilva.developer@gmail.com>
2024-06-23 23:08:25 -0400

    trying to solve mobile availability (it seems audio/mpeg wont run on mobile)

--- audiozzap-page/src/components/AudioPlayer.js
+++ audiozzap-page/src/components/AudioPlayer.js
@@ -1,15 +1,26 @@
-// AudioPlayer.js
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 const AudioPlayer = ({ src }) => {
+  // Check if src is defined
+  if (!src) {
+    return <div>No audio source provided</div>;
+  }
+
   return (
     <div>
       <audio controls>
         <source src={src} type="audio/mpeg" />
+        <source src={src.endsWith('.mp3') ? src.replace('.mp3', '.ogg') : src} type="audio/ogg" />
         Your browser does not support the audio element.
       </audio>
     </div>
   );
+};
+
+AudioPlayer.propTypes = {
+  src: PropTypes.string.isRequired,
 };
 
 export default AudioPlayer;
